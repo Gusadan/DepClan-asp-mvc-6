15d734f0f969d56da80e3b7db004095566cf83f3
Johannes Gansowen <jhans.ms@hotmail.com>
2016-05-30 20:23:58 -0400

    Add Views, Controllers, etc. for accessing and making use of Roles
    -Admin page was added.
    -code based from https://www.youtube.com/watch?v=GxRgaQdpjuw

---
+++ src/DepClan/Controllers/AdminController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DepClan.Models;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Identity;
+using DepClan.ViewModels.Admin;
+using Microsoft.AspNet.Authorization;
+
+namespace DepClan.Controllers
+{
+    public class AdminController : Controller
+    {
+        #region
+
+        UserManager<ApplicationUser> _userManager;
+        public DepContext _context;
+        public static List<AdminUserViewModel> usrList = new List<AdminUserViewModel>();
+        public static List<SelectListItem> roleList = new List<SelectListItem>();
+
+        #endregion
+
+        public AdminController(UserManager<ApplicationUser> userManager, DepContext depContext)
+        {
+            _userManager = userManager;
+            _context = depContext;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> ShowUserDetails(AdminUserViewModel model)
+        {
+            usrList.Clear();
+            IList<ApplicationUser> users = _userManager.Users.ToList();
+
+            foreach (var user in users)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                model.FirstName = user.FirstName;
+                foreach (var role in roles)
+                {
+                    model.RankName = role;
+                    switch (role)
+                    {
+                        case "Admin":
+                            model.RankId = "1"; break;
+                        case "Member":
+                            model.RankId = "2"; break;
+                    }
+                }
+
+                model.UserId = user.Id;
+                usrList.Add(new AdminUserViewModel() { FirstName = model.FirstName, RankName = model.RankName, UserId = model.UserId, RankId = model.RankId });
+                model.RankName = null;
+            }
+            return PartialView("ShowUserDetails");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult> Index(AdminUserViewModel model)
+        {
+            await ShowUserDetails(model);
+            return View();
+        }
+    }
+}
---
+++ src/DepClan/ViewModels/Admin/AdminUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepClan.ViewModels.Admin
+{
+    public class AdminUserViewModel
+    {
+        public string FirstName { get; set; }
+        
+        public string UserId { get; set; }
+
+        public string RankName { get; set; }
+
+        public string RankId { get; set; }
+    }
+}
